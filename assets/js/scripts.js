67868be5d5b4025a1bcc650d28d54351d146aed2
nicomts <nicolas.mateos7@etu-univ.lorraine.fr>
2019-05-09 17:43:36 +0200

    Import du Portfolio

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+$(function() {
+   		$("#bounce").click (function() { // Au clic sur un élément
+  var page = $(this).attr('href'); // Page cible
+  var speed = 750; // Durée de l'animation (en ms)
+  $('html, body').animate({
+  	scrollTop: $(page).offset().top
+  }, speed); // Go
+})
+   	});
